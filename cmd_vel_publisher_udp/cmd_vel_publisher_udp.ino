4dd26bbf08012acc746bafed910a0dc560085c05
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2023-04-30 19:30:48 +0530

    changed publisher QOS to default

--- cmd_vel_publisher_udp/cmd_vel_publisher_udp.ino
+++ cmd_vel_publisher_udp/cmd_vel_publisher_udp.ino
@@ -100,7 +100,7 @@ void setup() {
   RCCHECK(rclc_node_init_default(&node, "m5stack_controller_node", "", &support));
 
   // create publisher
-  RCCHECK(rclc_publisher_init_best_effort(
+  RCCHECK(rclc_publisher_init_default(
     &publisher,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
